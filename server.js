a90edb7bc7e74c0c1067c00730a739dab77e1a50
Dusty Ury <nesingwarysfinest@gmail.com>
2015-02-16 19:50:00 -0600

    modified ignores

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function(request, response) {
-    response.writeHead(200);
-    response.write('Hell this is dog.');
-    response.end();
-    setTimeout(function(){
-        response.write("Dog is finished!");
-        response.end();
-    }, 5000);
-}).listen(1337);
-console.log('Listening on 1337...');
+app.get('/', function(req, res) {
+    res.sendfile('index.html');
+});
+
+var server = app.listen(8080, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("Running at %s on port %s", host, port);
+})
